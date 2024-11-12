59874ea264f11bf74d1ca51eb56f3cb05f8e095f
bwire40 <emmanuelbwire80@gmail.com>
2024-11-12 09:08:04 +0300

    Started working on models and resources

--- resources/js/Pages/Profile/Index.jsx
+++ resources/js/Pages/Profile/Index.jsx
@@ -1,27 +1,28 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
+import React from "react";
 
-export default function Dashboard() {
-  //   console.log(auth);
+const Index = ({ auth, projects }) => {
   return (
     <AuthenticatedLayout
       header={
         <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-          Dashboard
+          Projects
         </h2>
       }
     >
-      <Head title="Dashboard" />
-
+      <Head title="Projects" />
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              You're logged in!
+              <pre>{JSON.stringify(projects, undefined, 2)}</pre>
             </div>
           </div>
         </div>
       </div>
     </AuthenticatedLayout>
   );
-}
+};
+
+export default Index;
